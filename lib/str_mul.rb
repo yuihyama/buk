9d06ff211d61e9831a434ba60f8e6fae658991c6
yuihyama <yuihyama@gmail.com>
2018-09-15 11:47:03 +0900

    Add file

---
+++ lib/str_mul.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+class String
+  def str_mul(times)
+    self * times
+  end
+end
+
+if __FILE__ == $0
+  p 'xxx'.str_mul(2) # => "xxxxxx"
+  puts 'abc'.str_mul(2) # => abcabc
+  # print 'xyz'.str_mul(-2) # => ArgumentError
+  print 'xyz'.str_mul(3.3), "\n" # => xyzxyzxyz
+end
