e9f808e3b1c0282d16172d9999c0d2da9d8e800b
yuihyama <yuihyama@gmail.com>
2018-09-16 10:05:23 +0900

    Add file

---
+++ lib/to_char.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+class String
+  def to_char
+    self.split(//)
+  end
+end
+
+if __FILE__ == $0
+  p 'a string'.to_char # => ["a", " ", "s", "t", "r", "i", "n", "g"]
+
+  puts 'a string'.to_char
+  # =>
+  # a
+  #
+  # s
+  # t
+  # r
+  # i
+  # n
+  # g
+
+  print 'a string'.to_char, "\n" # => ["a", " ", "s", "t", "r", "i", "n", "g"]
+end
