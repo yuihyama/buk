6f7c7b6ed0808add5bb0c07fe265f97eb6097396
yuihyama <yuihyama@gmail.com>
2018-09-16 10:05:02 +0900

    Add file

---
+++ test/to_char_test.rb
@@ -0,0 +1,18 @@
+require 'minitest/autorun'
+require './lib/to_char'
+
+class ToCharTest < Minitest::Test
+  def test_to_char
+    assert_equal ["a", " ", "s", "t", "r", "i", "n", "g"], 'a string'.to_char
+
+    assert_raises(NoMethodError) { 1.to_char }
+
+    assert_output("[\"a\", \" \", \"s\", \"t\", \"r\", \"i\", \"n\", \"g\"]\n") {
+      p 'a string'.to_char
+    }
+    assert_output("a\n \ns\nt\nr\ni\nn\ng\n") { puts 'a string'.to_char }
+    assert_output("[\"a\", \" \", \"s\", \"t\", \"r\", \"i\", \"n\", \"g\"]\n") {
+      print 'a string'.to_char, "\n"
+    }
+  end
+end
