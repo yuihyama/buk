f6e15a868e4f14c7e42f7d63a338b7f17576f7e3
yuihyama <yuihyama@gmail.com>
2018-10-02 09:45:59 +0900

    Clean to_char.rb

--- lib/to_char.rb
+++ lib/to_char.rb
@@ -2,12 +2,13 @@
 
 class String
   def to_char
-    self.split(//)
+    split(//)
   end
 end
 
 if __FILE__ == $0
-  p 'a string'.to_char # => ["a", " ", "s", "t", "r", "i", "n", "g"]
+  p 'a string'.to_char
+  # => ["a", " ", "s", "t", "r", "i", "n", "g"]
 
   puts 'a string'.to_char
   # =>
@@ -20,7 +21,9 @@ if __FILE__ == $0
   # n
   # g
 
-  print 'a string'.to_char, "\n" # => ["a", " ", "s", "t", "r", "i", "n", "g"]
+  print 'a string'.to_char, "\n"
+  # => ["a", " ", "s", "t", "r", "i", "n", "g"]
 
-  printf "%s\n", 'a string'.to_char # => ["a", " ", "s", "t", "r", "i", "n", "g"]
+  printf "%s\n", 'a string'.to_char
+  # => ["a", " ", "s", "t", "r", "i", "n", "g"]
 end
