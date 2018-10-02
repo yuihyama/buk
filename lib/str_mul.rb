7919e942092a26930a3126ff6f1df8d9857e9116
yuihyama <yuihyama@gmail.com>
2018-10-02 09:45:29 +0900

    Clean str_mul.rb

--- lib/str_mul.rb
+++ lib/str_mul.rb
@@ -8,7 +8,10 @@ end
 
 if __FILE__ == $0
   p 'xxx'.str_mul(2) # => "xxxxxx"
+
   puts 'abc'.str_mul(2) # => abcabc
+
   # print 'xyz'.str_mul(-2) # => ArgumentError
+
   print 'xyz'.str_mul(3.3), "\n" # => xyzxyzxyz
 end
